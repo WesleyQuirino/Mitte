e3892107c8c5971adf7072dfd43f9d168368d4e5
WesleyQuirino <wesleybathead@gmail.com>
2024-10-18 17:54:52 -0300

    .

--- src/PdfProcessor.php
+++ src/PdfProcessor.php
@@ -35,6 +35,38 @@ class PdfProcessor {
             return ['status' => 'error', 'message' => 'Erro: arquivo JSON não foi gerado.'];
         }
 
+        if (file_exists($jsonFile)) {
+            // Ler e decodificar o arquivo JSON
+            $json_data = json_decode(file_get_contents($jsonFile), true);
+            $processed_data = array();
+            $previous_item = array();
+
+            for ($i = 0; $i < count($json_data); $i++) {
+                if (empty($json_data[$i]["ID"])) {
+                    foreach ($json_data[$i] as $json_data_key => $json_data_value) {
+                        if (!empty($json_data_value)) {
+                            if (!empty($previous_item[$json_data_key])) {
+                                if (strpos($previous_item[$json_data_key], $json_data_value) === false) {
+                                    $previous_item[$json_data_key] .= " " . $json_data_value;
+                                }
+                            } else {
+                                $previous_item[$json_data_key] = $json_data_value;
+                            }
+                        }
+                    }
+                    $processed_data[count($processed_data) - 1] = $previous_item;
+                } else {
+                    $previous_item = $json_data[$i];
+                    $processed_data[] = $previous_item;
+                }
+            }
+            $jsonString = json_encode($processed_data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+
+            $file = fopen($jsonFile, 'w');
+            fwrite($file, $jsonString);
+            fclose($file);
+        }
+
         // Verifica se o arquivo JSON tem conteúdo
         $jsonContent = file_get_contents($jsonFile);
         if (empty($jsonContent)) {
